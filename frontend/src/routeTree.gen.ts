b0412dfda7d233c091ec34995eed0c4b8166a458
SaiSawant1 <saisawant2003@gmail.com>
2025-04-22 16:54:45 +0530

    deleted files in the front end

--- frontend/src/routeTree.gen.ts
+++
@@ -1,7 +0,0 @@
-import { FileRoute } from '@tanstack/react-router'
-import { Route as rootRoute } from './routes/__root'
-import { Route as indexRoute } from './routes/index'
-
-export const routeTree = rootRoute.addChildren([
-  indexRoute,
-]) 
--- frontend/src/routes/__root.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-
-export const Route = createRootRoute({
-  component: () => (
-    <div className="min-h-screen">
-      <Outlet />
-    </div>
-  ),
-}) 
